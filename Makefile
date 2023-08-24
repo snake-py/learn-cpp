3b7e5dddbb71dcc8cfe9e36922fb673e8850f074
f.batti <fabio.batti93@gmail.com>
2023-08-24 22:17:06 +0200

    init commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+run:
+	echo "Compiling..."
+	/usr/bin/g++ -fdiagnostics-color=always -g /home/fabio/Desktop/playground/c++/*.cpp -std=c++20 -o /home/fabio/Desktop/playground/c++/main
+	./main
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    if (20 > 18) {
+    cout << "20 is greater than 18";
+    }
+    auto result = (10 <=> 20) > 0; // result is false
+
+    return 0;
+}
